061bb33848f3ad84c291fe82707fa6bf8a2cafe8
Sean Murphy <sean@sdsc.ethz.ch>
2024-02-21 14:48:38 +0100

    chore(ztoc): modify cgo build directives to use pkg-config

--- ztoc/compression/gzip_zinfo.go
+++ ztoc/compression/gzip_zinfo.go
@@ -17,7 +17,7 @@
 package compression
 
 // #cgo CFLAGS: -I${SRCDIR}/
-// #cgo LDFLAGS: -L${SRCDIR}/../out -l:libz.a
+// #cgo pkg-config: zlib
 // #include "gzip_zinfo.h"
 // #include <stdlib.h>
 // #include <stdint.h>
